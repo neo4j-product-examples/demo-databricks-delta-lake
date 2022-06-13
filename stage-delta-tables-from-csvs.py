b95e13b5c2031583ae73e890315758fa967894ac
zach.blumenfeld@neo4j.com <zach.blumenfeld@neo4j.com>
2022-06-13 04:57:28 +0000

    Quick Grammar Fix

--- stage-delta-tables-from-csvs.py
+++ stage-delta-tables-from-csvs.py
@@ -6,7 +6,7 @@
 # MAGIC 1. Download `articles.csv`, `customers.csv`, and `transaction_train.csv` from the Kaggle page: https://www.kaggle.com/competitions/h-and-m-personalized-fashion-recommendations/data
 # MAGIC 2. create directory `/FileStore/hm-data/` in the DBFS and upload csvs to that directory. 
 # MAGIC    1. `articles.csv` and `customers.csv` can be uploaded through either the UI or Databricks CLI
-# MAGIC    2. `transaction_train.csv` must be uploaded through the Databricks CLI due to file size. The general command syntax is: `databricks fs cp transactions_train.csv dbfs:/FileStore/hm-data/transactions_train.csv`. Note it may take a while (like an hour)
+# MAGIC    2. `transaction_train.csv` must be uploaded through the Databricks CLI due to file size. The general command syntax is: `databricks fs cp transactions_train.csv dbfs:/FileStore/hm-data/transactions_train.csv`. Note it may take a while (like an hour).
 
 # COMMAND ----------
 
